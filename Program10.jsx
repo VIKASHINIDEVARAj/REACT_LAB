9ef9803eeedf02a875571225aa040d4de3b4c4bd
VIKASHINIDEVARAj <vikashinidevaraj27@gmail.com>
2026-02-18 10:09:56 +0530

    Fix newline at end of Home.jsx
    Added a newline at the end of the Home component file.

--- Program10.jsx
+++ Program10.jsx
@@ -14,3 +14,18 @@ createRoot(document.getElementById('root')).render(
    </BrowserRouter> 
   </StrictMode>,
 )
+
+Home.jsx
+import React from 'react'
+import {Link} from "react-router-dom"
+function Home() {
+  return (
+    <div>
+        <h3>Hi! Click here to go to the page: Post.jsx 
+            <Link to="/post"> Go</Link></h3>
+    </div>
+  )
+}
+
+export default Home
+
