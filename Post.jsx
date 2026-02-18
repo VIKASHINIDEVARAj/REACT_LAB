0938575132a9dc2af0182dd2c8c02af657ec655d
VIKASHINIDEVARAj <vikashinidevaraj27@gmail.com>
2026-02-18 09:52:13 +0530

    Add files via upload

---
+++ Post.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Post() {
+  return (
+    <div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, inventore suscipit neque 
+            nesciunt sint eligendi aspernatur, dicta labore voluptates sapiente a ex eos ratione
+             molestiae maiores natus. Modi, odit consectetur?</p>
+    </div>
+  )
+}
+
+export default Post
